eec0ea0a726e54511c048b22ffd4037ea0c4ab14
chun77 <zichunwang733@gmail.com>
2024-09-29 23:11:23 -0400

    3.11.1

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,11 @@ def test_os_and_sys_version():
     python_version, os_name = os_and_sys_version()
     python_version_check = ["3.7", "3.8", "3.9", "3.10", "3.11"]
     os_name_check = ["Windows", "Linux"]
-    assert python_version in python_version_check
+
+    # Allowing any version starting with 3.7, 3.8, 3.9, 3.10, or 3.11
+    major_version = ".".join(python_version.split(".")[:2])  # Only keep major and minor version
+    assert major_version in python_version_check
+    
     # if environment has an OS installed
     if os_name is not None:
         assert os_name in os_name_check
